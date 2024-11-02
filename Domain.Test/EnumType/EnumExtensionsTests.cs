ed0f37dc9e8d36bff935b48a73c8898f9e246db4
gabrielnino <gabrielnino@gmail.com>
2024-11-02 16:07:32 -0700

    Fix.

--- Domain.Test/EnumType/EnumExtensionsTests.cs
+++ Domain.Test/EnumType/EnumExtensionsTests.cs
@@ -120,5 +120,80 @@ namespace Domain.Test.EnumType
             // Act & Assert
             Assert.ThrowsException<ArgumentException>(() => value.GetEnumByName<MyEnum>());
         }
+
+        private enum TestEnum
+        {
+            [EnumMetadata("TestName1", "This is the first test enum.")]
+            TestValue1,
+
+            [EnumMetadata("TestName2", "This is the second test enum.")]
+            TestValue2,
+
+            TestValueWithoutMetadata
+        }
+
+        [TestMethod]
+        public void GetCustomName_Should_Return_Correct_Custom_Name_When_Metadata_Is_Present()
+        {
+            // Act
+            var customName1 = TestEnum.TestValue1.GetCustomName();
+            var customName2 = TestEnum.TestValue2.GetCustomName();
+
+            // Assert
+            Assert.AreEqual("TestName1", customName1);
+            Assert.AreEqual("TestName2", customName2);
+        }
+
+        [TestMethod]
+        public void GetCustomName_Should_Return_Unknown_When_Metadata_Is_Not_Present()
+        {
+            // Act
+            var customName = TestEnum.TestValueWithoutMetadata.GetCustomName();
+
+            // Assert
+            Assert.AreEqual("UNKNOWN", customName);
+        }
+
+        [TestMethod]
+        public void GetDescription_Should_Return_Correct_Description_When_Metadata_Is_Present()
+        {
+            // Act
+            var description1 = TestEnum.TestValue1.GetDescription();
+            var description2 = TestEnum.TestValue2.GetDescription();
+
+            // Assert
+            Assert.AreEqual("This is the first test enum.", description1);
+            Assert.AreEqual("This is the second test enum.", description2);
+        }
+
+        [TestMethod]
+        public void GetDescription_Should_Return_Default_Message_When_Metadata_Is_Not_Present()
+        {
+            // Act
+            var description = TestEnum.TestValueWithoutMetadata.GetDescription();
+
+            // Assert
+            Assert.AreEqual("Description not available.", description);
+        }
+
+        [TestMethod]
+        public void GetEnumByName_Should_Return_Correct_Enum_Value_For_Valid_String()
+        {
+            // Act
+            var enumValue1 = "TestValue1".GetEnumByName<TestEnum>();
+            var enumValue2 = "TestValue2".GetEnumByName<TestEnum>();
+
+            // Assert
+            Assert.AreEqual(TestEnum.TestValue1, enumValue1);
+            Assert.AreEqual(TestEnum.TestValue2, enumValue2);
+        }
+
+        [TestMethod]
+        public void GetEnumByName_Should_Throw_ArgumentException_For_Invalid_String()
+        {
+            // Act & Assert
+            var exception = Assert.ThrowsException<ArgumentException>(() => "InvalidValue".GetEnumByName<TestEnum>());
+            Assert.AreEqual("No enum value found for InvalidValue in Domain.Test.EnumType.EnumExtensionsTests+TestEnum", exception.Message);
+        }
     }
 }
--- Domain.Test/EnumType/EnumMetadataAttributeTests.cs
+++ Domain.Test/EnumType/EnumMetadataAttributeTests.cs
@@ -58,5 +58,54 @@ namespace Domain.Test.EnumType
         {
             Assert.AreEqual(_description, _testClass.Description);
         }
+
+        [TestMethod]
+        public void EnumMetadataAttribute_Should_Store_Name_And_Description_Correctly()
+        {
+            // Arrange
+            string expectedName = "TestName";
+            string expectedDescription = "This is a test description for the enum field.";
+
+            // Act
+            var enumMetadataAttribute = new EnumMetadataAttribute(expectedName, expectedDescription);
+
+            // Assert
+            Assert.AreEqual(expectedName, enumMetadataAttribute.Name);
+            Assert.AreEqual(expectedDescription, enumMetadataAttribute.Description);
+        }
+
+        [TestMethod]
+        public void EnumMetadataAttribute_Should_Throw_No_Exception_When_Initialized_With_Valid_Parameters()
+        {
+            // Act & Assert
+            try
+            {
+                new EnumMetadataAttribute("ValidName", "Valid description.");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("An exception was thrown when initializing with valid parameters: " + ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void EnumMetadataAttribute_Should_Be_Applied_To_Enum_Fields()
+        {
+            // Arrange & Act
+            var enumType = typeof(TestEnumWithMetadata);
+            var fieldInfo = enumType.GetField(nameof(TestEnumWithMetadata.TestValue));
+
+            // Assert
+            var attribute = (EnumMetadataAttribute)Attribute.GetCustomAttribute(fieldInfo, typeof(EnumMetadataAttribute));
+            Assert.IsNotNull(attribute);
+            Assert.AreEqual("TestValueName", attribute.Name);
+            Assert.AreEqual("This is a description for TestValue.", attribute.Description);
+        }
+
+        private enum TestEnumWithMetadata
+        {
+            [EnumMetadata("TestValueName", "This is a description for TestValue.")]
+            TestValue
+        }
     }
 }
