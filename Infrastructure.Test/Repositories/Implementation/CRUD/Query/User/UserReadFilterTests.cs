716f126ca6ae65f8b3edfb0effba0cad4c64fc56
gabrielnino <gabrielnino@gmail.com>
2024-11-06 18:20:48 -0800

    Fix.

--- Infrastructure.Test/Repositories/Implementation/CRUD/Query/User/UserReadFilterTests.cs
+++ Infrastructure.Test/Repositories/Implementation/CRUD/Query/User/UserReadFilterTests.cs
@@ -1,14 +1,9 @@
 namespace Infrastructure.Test.Repositories.Implementation.CRUD.Query.User
 {
-    using System;
     using Application.UseCases.ExternalServices;
     using Infrastructure.Repositories.Implementation.CRUD.Query.User;
-    using Microsoft.EntityFrameworkCore;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Moq;
-    using Persistence.BaseDbContext;
-    using Persistence.CreateStruture.Constants.ColumnType;
-    using TContext = System.String;
 
     [TestClass]
     public class UserReadFilterTests : TestsBase
