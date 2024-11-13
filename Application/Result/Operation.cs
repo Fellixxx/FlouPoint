adabc99ebc0c0a0ffd5209785ecf418465ca27a2
gabrielnino <gabrielnino@gmail.com>
2024-11-12 20:02:54 -0800

    Refactor operator.

--- Application/Result/Operation.cs
+++ Application/Result/Operation.cs
@@ -3,30 +3,39 @@
     using Application.Result.Error;
     using Application.Result.Exceptions;
 
+    /// <summary>
+    /// Represents the outcome of an operation with a result of type <typeparamref name="T"/>.
+    /// Provides methods to create both successful and failed operations.
+    /// </summary>
     public class Operation<T> : Result<T>
     {
-        private Operation()
-        {
-
-        }
-
         private const string InvalidOperation = "This method can only be used if the value of IsSuccessful is false.";
+        /// <summary>
+        /// Private constructor to prevent direct instantiation. Use factory methods <see cref="Success"/> or <see cref="Failure"/>.
+        /// </summary>
+        private Operation() { }
+
+
 
         /// <summary>
-        /// Checks if the current operation result indicates a failure.
-        /// Throws an exception if the operation was successful.
+        /// Ensures the operation represents a failure; otherwise, throws an <see cref="InvalidOperation"/>.
         /// </summary>
+        /// <exception cref="InvalidOperation">Thrown if the operation was successful.</exception>
+
         private void EnsureIsFailure()
         {
-            if (IsSuccessful.Equals(true))
+            if (IsSuccessful)
             {
                 throw new InvalidOperation(InvalidOperation);
             }
         }
 
         /// <summary>
-        /// Creates a new OperationResult with the specified generic type based on the current result.
+        /// Converts the current failed operation result to a specified generic type.
+        /// Only allowed if the current operation is a failure.
         /// </summary>
+        /// <typeparam name="U">The target result type.</typeparam>
+        /// <returns>A new <see cref="Operation{U}"/> with failure status.</returns>
         public Operation<U> AsType<U>()
         {
             EnsureIsFailure();
@@ -41,9 +50,9 @@
         /// <summary>
         /// Creates a successful operation result with the given data and optional message.
         /// </summary>
-        /// <param name="data">Data result return</param>
-        /// <param name="message">The message</param>
-        /// <returns>The operation result</returns>
+        /// <param name="data">The result data.</param>
+        /// <param name="message">Optional message describing the result.</param>
+        /// <returns>A successful <see cref="Operation{T}"/> instance.</returns>
         public static Operation<T> Success(T? data, string? message = "")
         {
             return new Operation<T>
@@ -55,20 +64,29 @@
             };
         }
 
-        /// <summary>
-        /// Converts the current result to a boolean type.
-        /// </summary>
-        public Operation<U> ConvertTo<U>() => AsType<U>();
 
         /// <summary>
-        /// Creates a failed operation result with the given message and error type.
+        /// Creates a failed operation result with the specified message and error type.
         /// </summary>
-        /// <param name="message">The message</param>
-        /// <param name="errorTypes">The error type</param>
-        /// <returns>The operation result</returns>
+        /// <param name="message">The failure message.</param>
+        /// <param name="errorTypes">The type of error encountered.</param>
+        /// <returns>A failed <see cref="Operation{T}"/> instance.</returns>
         public static Operation<T> Failure(string message, ErrorTypes errorTypes)
         {
-            return new Operation<T> { IsSuccessful = false, Message = message, Type = errorTypes };
+            return new Operation<T>
+            {
+                IsSuccessful = false,
+                Message = message,
+                Type = errorTypes
+            };
         }
+
+        /// <summary>
+        /// Converts the current operation result to a specified generic type.
+        /// Only allowed if the operation represents a failure.
+        /// </summary>
+        /// <typeparam name="U">The target type to convert to.</typeparam>
+        /// <returns>A new <see cref="Operation{U}"/> with the same failure status.</returns>
+        public Operation<U> ConvertTo<U>() => AsType<U>();
     }
 }
